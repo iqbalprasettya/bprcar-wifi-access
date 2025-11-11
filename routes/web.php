d89436cbd62f1a361ec286c50c477f495828d3b9
Iqbal Prasetya <muhamadiqbalprasetya89@gmail.com>
2025-11-11 09:23:19 +0700

    Update routes to streamline portal access and maintain hotspot management functionality

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HotspotAdminController;
 use App\Http\Controllers\PortalController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route utama langsung ke portal login
+Route::get('/', [PortalController::class, 'show'])->name('portal.show');
+Route::post('/login', [PortalController::class, 'submit'])->name('portal.submit');
 
-
-Route::get ('/portal', [PortalController::class,'show'])->name('portal.show');
-Route::post('/portal/login', [PortalController::class,'submit'])->name('portal.submit');
-
-Route::get ('/admin/hotspot/users', [HotspotAdminController::class,'index']);
-Route::post('/admin/hotspot/users', [HotspotAdminController::class,'store']);
-Route::patch('/admin/hotspot/users/{name}', [HotspotAdminController::class,'update']);
-Route::delete('/admin/hotspot/users/{name}', [HotspotAdminController::class,'destroy']);
-Route::get ('/admin/hotspot/active', [HotspotAdminController::class,'active']);
-Route::post('/admin/hotspot/kick/{user}', [HotspotAdminController::class,'kick']);
+Route::get('/admin/hotspot/users', [HotspotAdminController::class, 'index']);
+Route::post('/admin/hotspot/users', [HotspotAdminController::class, 'store']);
+Route::patch('/admin/hotspot/users/{name}', [HotspotAdminController::class, 'update']);
+Route::delete('/admin/hotspot/users/{name}', [HotspotAdminController::class, 'destroy']);
+Route::get('/admin/hotspot/active', [HotspotAdminController::class, 'active']);
+Route::post('/admin/hotspot/kick/{user}', [HotspotAdminController::class, 'kick']);
